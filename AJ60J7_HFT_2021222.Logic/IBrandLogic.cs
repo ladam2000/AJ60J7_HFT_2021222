e695c555cf30f7c03fa3928dae40f05aa04a0eb1
László Ádám <adam00swim@gmail.com>
2022-04-23 14:42:26 +0200

    IBrandLogic interface keszites

---
+++ AJ60J7_HFT_2021222.Logic/IBrandLogic.cs
@@ -0,0 +1,19 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public interface IBrandLogic
+    {
+        IEnumerable<KeyValuePair<string, double>> BrandPopularity();
+        void Create(Brand brand);
+        void Delete(int brandId);
+        IEnumerable<Brand> ReadAll();
+        Brand ReadOne(int id);
+        void Update(Brand brand);
+    }
+}
