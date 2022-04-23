8e9da1ea5e025ea90a6b2c6e17e77fdda3684d5e
László Ádám <adam00swim@gmail.com>
2022-04-23 14:50:22 +0200

    add ICarLogic Interface

---
+++ AJ60J7_HFT_2021222.Logic/ICarLogic.cs
@@ -0,0 +1,23 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public interface ICarLogic
+    {
+        double HighestPrice();
+        double AveragePrice();
+        IEnumerable<KeyValuePair<string, double>> AveragePBB();
+        IEnumerable<KeyValuePair<string, double>> AveragePriceByEngineTypes();
+        IEnumerable<KeyValuePair<string, double>> EngineTypeUsage();
+        void Create(Car car);
+        IEnumerable<Car> ReadAll();
+        Car ReadOne(int id);
+        void Update(Car car);
+        void Delete(int carId);
+    }
+}
