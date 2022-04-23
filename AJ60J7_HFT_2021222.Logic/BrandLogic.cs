ca429bf6ea41a2e16a6bf70f1b6933d7e8b712a7
László Ádám <adam00swim@gmail.com>
2022-04-23 17:26:51 +0200

    BrandLogic class, IBrandLogic implement

---
+++ AJ60J7_HFT_2021222.Logic/BrandLogic.cs
@@ -0,0 +1,42 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public class BrandLogic : IBrandLogic
+    {
+        public IEnumerable<KeyValuePair<string, double>> BrandPopularity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int brandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Brand> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand ReadOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
