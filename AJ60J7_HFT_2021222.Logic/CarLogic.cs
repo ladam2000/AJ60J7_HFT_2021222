dbe9c592440181de6ee8fe12a16af92c3d4e7d40
László Ádám <adam00swim@gmail.com>
2022-04-23 17:25:36 +0200

    CarLogic class, ICarlogic implement

---
+++ AJ60J7_HFT_2021222.Logic/CarLogic.cs
@@ -0,0 +1,62 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public class CarLogic : ICarLogic
+    {
+        public IEnumerable<KeyValuePair<string, double>> AveragePBB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double AveragePrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, double>> AveragePriceByEngineTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, double>> EngineTypeUsage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double HighestPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car ReadOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
