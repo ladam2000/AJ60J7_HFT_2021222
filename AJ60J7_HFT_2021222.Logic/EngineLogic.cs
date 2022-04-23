c1a7760405127d2a5f332d97ca4ffd87623d602e
László Ádám <adam00swim@gmail.com>
2022-04-23 17:28:57 +0200

    EngineLogic class, IEngineLogic implement

---
+++ AJ60J7_HFT_2021222.Logic/EngineLogic.cs
@@ -0,0 +1,42 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public class EngineLogic : IEngineLogic
+    {
+        public void Create(Engine engine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int engineId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, int?>> DieselCostHigherThan4k()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Engine> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Engine ReadOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Engine engine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
