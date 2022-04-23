b604743a12c711b17505c8b044d014e9a70035ba
László Ádám <adam00swim@gmail.com>
2022-04-23 17:22:54 +0200

    IEngineLogic Interface

---
+++ AJ60J7_HFT_2021222.Logic/IEngineLogic.cs
@@ -0,0 +1,19 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Logic
+{
+    public interface IEngineLogic
+    {
+        IEnumerable<KeyValuePair<string, int?>> DieselCostHigherThan4k();
+        void Create(Engine engine);
+        void Delete(int engineId);
+        IEnumerable<Engine> ReadAll();
+        Engine ReadOne(int id);
+        void Update(Engine engine);
+    }
+}
