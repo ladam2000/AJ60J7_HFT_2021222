6ff03f6a3df7b8e72eee0a5827560573719353a0
László Ádám <adam00swim@gmail.com>
2022-05-03 19:45:27 +0200

    f1.0

---
+++ AJ60J7_HFT_2021222.Endpoint/EngineController.cs
@@ -0,0 +1,56 @@
+﻿using AJ60J7_HFT_2021222.Logic;
+using AJ60J7_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Endpoint
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EngineController : ControllerBase
+    {
+        IEngineLogic logic;
+
+        public EngineController(IEngineLogic lc)
+        {
+            this.logic = lc;
+        }
+
+        // GET: api/<EngineController>
+        [HttpGet]
+        public IEnumerable<Engine> GetAll()
+        {
+            return logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Engine GetOne(int id)
+        {
+            return logic.ReadOne(id);
+        }
+
+        // POST api/<EngineController>
+        [HttpPost]
+        public void CreateOne([FromBody] Engine value)
+        {
+            logic.Create(value);
+        }
+
+        // PUT api/<EngineController>/5
+        [HttpPut]
+        public void UpdateOne([FromBody] Engine value)
+        {
+            logic.Update(value);
+        }
+
+        // DELETE api/<EngineController>/5
+        [HttpDelete("{id}")]
+        public void DeleteOne(int id)
+        {
+            logic.Delete(id);
+        }
+    }
+}
---
+++ AJ60J7_HFT_2021222.Endpoint/StatController.cs
@@ -0,0 +1,71 @@
+﻿using AJ60J7_HFT_2021222.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Endpoint
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        ICarLogic cl;
+        IBrandLogic bl;
+        IEngineLogic el;
+
+        public StatController(ICarLogic cl, IBrandLogic bl, IEngineLogic el)
+        {
+            this.cl = cl;
+            this.bl = bl;
+            this.el = el;
+        }
+
+        // GET: /stat/averagePBB
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> AveragePBB()
+        {
+            return cl.AveragePBB();
+        }
+
+        // GET: /stat/AveragePriceByEngineTypes
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> AveragePriceByEngineTypes()
+        {
+            return cl.AveragePriceByEngineTypes();
+        }
+
+        // GET: /stat/EngineTypeUsage
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> EngineTypeUsage()
+        {
+            return cl.EngineTypeUsage();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> BrandPopularity()
+        {
+            return bl.BrandPopularity();
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int?>> DieselCostHigherThan4k()
+        {
+            return el.DieselCostHigherThan4k();
+        }
+
+        [HttpGet]
+        public double AveragePrice()
+        {
+            return cl.AveragePrice();
+        }
+
+        [HttpGet]
+        public double HighestPrice()
+        {
+            return cl.HighestPrice();
+        }
+
+    }
+}
