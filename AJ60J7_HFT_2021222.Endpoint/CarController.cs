ff97e71cbea17d11e3913a3bcc264c427e751de3
László Ádám <adam00swim@gmail.com>
2022-05-03 19:38:43 +0200

    CarController

---
+++ AJ60J7_HFT_2021222.Endpoint/CarController.cs
@@ -0,0 +1,60 @@
+﻿using AJ60J7_HFT_2021222.Logic;
+using AJ60J7_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJ60J7_HFT_2021222.Endpoint
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+
+        ICarLogic logic;
+
+        public CarController(ICarLogic cl)
+        {
+            this.logic = cl;
+        }
+
+        // GET: /car
+        [HttpGet]
+        public IEnumerable<Car> GetAll()
+        {
+            return logic.ReadAll();
+        }
+
+        //GET /car/{id}
+        [HttpGet("{id}")]
+        public Car GetOne(int id)
+        {
+            return logic.ReadOne(id);
+        }
+
+        // POST /car
+        //Create
+        [HttpPost]
+        public void AddOne([FromBody] Car value)
+        {
+            logic.Create(value);
+        }
+
+        // PUT /car
+        //Update
+        [HttpPut]
+        public void EditOne([FromBody] Car value)
+        {
+            logic.Update(value);
+        }
+
+        // DELETE /car/{carId}
+        [HttpDelete("{id}")]
+        public void DeleteOne([FromRoute] int carId)
+        {
+            logic.Delete(carId);
+        }
+    }
+}
