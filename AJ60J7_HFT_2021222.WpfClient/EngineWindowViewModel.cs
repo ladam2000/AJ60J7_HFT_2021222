7598736f375f6dea47c0b918004476a7df6ee8cf
Adam Laszlo <adam00swim@gmail.com>
2022-10-23 20:05:56 +0200

    engine view model

---
+++ AJ60J7_HFT_2021222.WpfClient/EngineWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AJ60J7_HFT_2021222.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AJ60J7_HFT_2021222.WpfClient
+{
+    public class EngineWindowViewModel
+    {
+    }
+}
