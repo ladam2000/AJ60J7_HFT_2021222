d02be57c071c5f445e6d1c9554244a35a1ab6f7d
Adam Laszlo <adam00swim@gmail.com>
2022-11-17 17:17:28 +0100

    index.html id name

--- AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
+++ AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,3 +1,19 @@
-﻿fetch('http://localhost:44728/brand')
+﻿let brands = [];
+
+fetch('http://localhost:44728/brand')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        brands = y;
+        console.log(brands);
+        display();
+    });
+
+function display() {
+    brands.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td></tr>"
+        console.log(t.name)
+    });
+
+}
