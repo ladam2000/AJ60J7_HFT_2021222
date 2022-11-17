593bd780ac2d366a5c87ccb8f99d81cf061e5191
Adam Laszlo <adam00swim@gmail.com>
2022-11-17 18:00:46 +0100

    hibas

--- AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
+++ AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,12 +1,15 @@
 ﻿let brands = [];
+getdata();
 
-fetch('http://localhost:44728/brand')
-    .then(x => x.json())
-    .then(y => {
-        brands = y;
-        console.log(brands);
-        display();
-    });
+async function getdata() {
+    await fetch('http://localhost:44728/brand')
+        .then(x => x.json())
+        .then(y => {
+            brands = y;
+            console.log(brands);
+            display();
+        });
+}
 
 function display() {
     brands.forEach(t => {
@@ -15,5 +18,19 @@ function display() {
             + t.name + "</td></tr>"
         console.log(t.name)
     });
+}
 
+function create() {
+    let brandName = document.getElementById('brandname').value;
+    fetch('http://localhost:44728/brand', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { name: brandName })})
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
