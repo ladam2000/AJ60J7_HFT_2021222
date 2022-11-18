647749567283fd9a9c2e89de42c1fc1abab7f980
Adam Laszlo <adam00swim@gmail.com>
2022-11-18 23:57:19 +0100

    js remove not working

--- AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
+++ AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
@@ -2,31 +2,49 @@
 getdata();
 
 async function getdata() {
-    await fetch('http://localhost:44728/brand')
+    await fetch('http://localhost:44728/brand/')
         .then(x => x.json())
         .then(y => {
             brands = y;
-            console.log(brands);
             display();
         });
 }
 
+
+
 function display() {
+    document.getElementById('resultarea').innerHTML = "";
     brands.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>"
-            + t.name + "</td></tr>"
-        console.log(t.name)
+            + t.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.id})">Delete</button>`
+            + "</td></tr>";
     });
 }
+function remove(id) {
+    fetch('http://localhost:44728/brand/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
 
 function create() {
     let brandName = document.getElementById('brandname').value;
-    fetch('http://localhost:44728/brand', {
+    fetch('http://localhost:44728/brand/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { name: brandName })})
+            { brandname: brandName })})
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
