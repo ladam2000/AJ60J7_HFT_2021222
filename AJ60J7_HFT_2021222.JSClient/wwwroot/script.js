d91063174a24b5ae95aa3247cc5e6de4362deeae
Adam Laszlo <adam00swim@gmail.com>
2022-11-19 14:05:22 +0100

    js SignalR

--- AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
+++ AJ60J7_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,17 +1,48 @@
 ﻿let brands = [];
+let connection = null;
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:44728/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("BrandCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("BrandDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
 
 async function getdata() {
-    await fetch('http://localhost:44728/brand')
+    await fetch('http://localhost:44728/brand/')
         .then(x => x.json())
         .then(y => {
             brands = y;
-            console.log(brands);
+            //console.log(brands);
             display();
         });
 }
-
-
 
 function display() {
     document.getElementById('resultarea').innerHTML = "";
@@ -23,27 +54,24 @@ function display() {
             + "</td></tr>";
     });
 }
+
 function remove(id) {
-    fetch('http://localhost:44728/brand' + id, {
+    fetch('http://localhost:44728/brand/' + id, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json', },
-        body: null
-    })
+        body: null })
         .then(response => response)
         .then(data => {
-           
             console.log('Success:', data);
-            
             getdata();
         })
         .catch((error) => { console.error('Error:', error); });
 
 }
 
-
 function create() {
     let brandName = document.getElementById('brandname').value;
-    fetch('http://localhost:44728/brand', {
+    fetch('http://localhost:44728/brand/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
